9d5ba21ba78980fdf34016e947efe803197f9af2
Adam Leathorn <Adam.Leathorn@canadalife.com>
2022-07-06 14:30:59 -0400

    Add Special Pythagorean Triplet problem

---
+++ Euler9SpecialPythagoreanTriplet/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Euler9SpecialPythagoreanTriplet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1, b = 2, c = 3;
+            int x = 1000;
+
+            GetTriplet(ref a, ref b, ref c, x);
+
+            Console.WriteLine((a * b * c).ToString());
+        }
+
+        private static void GetTriplet(ref int a, ref int b, ref int c, int x)
+        {
+            for (a = 1; a < x - 2; a++)
+            {
+                for (b = 2; b < x - 1; b++)
+                {
+                    for (c = 3; c < x; c++)
+                    {
+                        if ((a + b + c == x) && ((a * a) + (b * b) == (c * c)))
+                        {
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
